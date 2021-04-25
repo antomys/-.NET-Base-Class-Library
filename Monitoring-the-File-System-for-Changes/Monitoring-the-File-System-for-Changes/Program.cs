1a84efac2611fcb81f5efdfad8cf48ae91ec8931
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-04-25 19:11:26 +0300

    Starting module 2

---
+++ Monitoring-the-File-System-for-Changes/Monitoring-the-File-System-for-Changes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monitoring_the_File_System_for_Changes
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
