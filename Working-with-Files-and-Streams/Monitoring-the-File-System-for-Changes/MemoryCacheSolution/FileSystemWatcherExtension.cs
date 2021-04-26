bcef7280eae55da2709c3b80f3f8139f838b4e00
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-04-27 00:07:53 +0300

    Implemented MemoryCacheSolution.csproj for monitoring file system

---
+++ Working-with-Files-and-Streams/Monitoring-the-File-System-for-Changes/MemoryCacheSolution/FileSystemWatcherExtension.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Caching;
+using CustomListSolution;
+
+namespace MemoryCacheSolution
+{
+    internal static class FileSystemWatcherExtension
+    {
+        // Implementing bag to eliminate processing duplicates
+        private static readonly MemoryCache MemoryCache = MemoryCache.Default;
+        internal static void FileSystemWatcherOnDisposed(object sender, EventArgs e)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: Disposed {e}");
+        }
+
+        internal static void FileSystemWatcherOnError(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: Occured error {e.GetException().Message}");
+        }
+
+        internal static void FileSystemWatcherOnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: Renamed file {e.OldName} to {e.Name}");
+            e.FullPath.Process();
+        }
+
+        internal static void FileSystemWatcherOnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: Deleted file {e.Name} in {e.FullPath}");
+        }
+
+        internal static void FileSystemWatcherOnCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: Created file {e.Name} in {e.FullPath}");
+            AddToCache(e.FullPath);
+        }
+
+        private static void Process(this string fullPath)
+        {
+            var fileName = Path.GetFileName(fullPath);
+            
+            if (File.GetAttributes(fullPath).HasFlag(FileAttributes.Directory))
+                return;
+            
+            if (string.IsNullOrEmpty(fullPath))
+                throw new ArgumentNullException(fullPath);
+            
+            if (MemoryCache.Contains(fileName)) return;
+            
+            var processFile = new FileProcessor(fullPath);
+            processFile.ProcessFile();
+        }
+
+        internal static void FileSystemWatcherOnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: Changed file {e.Name}");
+            AddToCache(e.FullPath);
+        }
+
+        private static void AddToCache(this string fullPath)
+        {
+            var newItem = new CacheItem(Path.GetFileNameWithoutExtension(fullPath), fullPath);
+            var cachingPolicy = new CacheItemPolicy
+            {
+                RemovedCallback = ProcessFile,
+                SlidingExpiration = TimeSpan.FromSeconds(2)
+            };
+            MemoryCache.Add(newItem,cachingPolicy);
+        }
+
+        private static void ProcessFile(CacheEntryRemovedArguments removedArguments)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: Removed item {removedArguments.CacheItem}. Reason: {removedArguments.RemovedReason}");
+
+            if (removedArguments.RemovedReason == CacheEntryRemovedReason.Expired)
+            {
+                var processor = new FileProcessor(removedArguments.CacheItem.Value.ToString());
+                processor.ProcessFile();
+            }
+            else
+            {
+                Console.WriteLine($"Unexpected reason removal: {removedArguments.CacheItem.Key}");
+            }
+        }
+    }
+}
--- Working-with-Files-and-Streams/Monitoring-the-File-System-for-Changes/MemoryCacheSolution/Program.cs
+++ Working-with-Files-and-Streams/Monitoring-the-File-System-for-Changes/MemoryCacheSolution/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace MemoryCacheSolution
 {
@@ -6,7 +7,38 @@ namespace MemoryCacheSolution
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length == 0)
+                return;
+            args[0].Init();
+        }
+        
+        private static void Init(this string directoryName)
+        {
+            if (!Directory.Exists(directoryName))
+                throw new ArgumentNullException(directoryName);
+            using var fileSystemWatcher = new FileSystemWatcher(directoryName)
+            {
+                IncludeSubdirectories = false, 
+                Filter = "*.*",
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite,
+                // Without this it will not generate events. By default - false.
+                EnableRaisingEvents = true
+            };
+
+            fileSystemWatcher.InitializeMethods();
+            
+            Console.WriteLine("Beginning to watch\nPress any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static void InitializeMethods(this FileSystemWatcher fileSystemWatcher)
+        {
+            fileSystemWatcher.Created += FileSystemWatcherExtension.FileSystemWatcherOnCreated;
+            fileSystemWatcher.Deleted += FileSystemWatcherExtension.FileSystemWatcherOnDeleted;
+            fileSystemWatcher.Renamed += FileSystemWatcherExtension.FileSystemWatcherOnRenamed;
+            fileSystemWatcher.Error += FileSystemWatcherExtension.FileSystemWatcherOnError;
+            fileSystemWatcher.Disposed += FileSystemWatcherExtension.FileSystemWatcherOnDisposed;
+            fileSystemWatcher.Changed += FileSystemWatcherExtension.FileSystemWatcherOnChanged;
         }
     }
 }
