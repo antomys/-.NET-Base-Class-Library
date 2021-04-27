69911e3942fac17e9d746ba6973c2070cffdf25b
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-04-28 00:15:49 +0300

    Created Reading-and-Writing-Entire-Files-into-Memory.sln

---
+++ Working-with-Files-and-Streams/Reading-and-Writing-Entire-Files-into-Memory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reading_and_Writing_Entire_Files_into_Memory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
