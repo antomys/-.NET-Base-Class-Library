61987b995e4f2951fec73bfa2f74c6a1f34ad3a8
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-04-28 23:51:03 +0300

    Implemented three methods to read files

---
+++ Working-with-Files-and-Streams/Reading-and-Writing-Entire-Files-into-Memory/TextFileProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reading_and_Writing_Entire_Files_into_Memory
+{
+    public class TextFileProcessor
+    {
+        private string InputFileName { get; init; }
+        private string OutputFileName { get; init; }
+
+        public TextFileProcessor(string inputFileName, string outputFileName)
+        {
+            InputFileName = inputFileName;
+            OutputFileName = outputFileName;
+        }
+
+        public async Task ProcessAsWholeString()
+        {
+            await File.WriteAllTextAsync(OutputFileName,
+                (await File.ReadAllTextAsync(InputFileName)).ToUpper());
+            File.Delete(InputFileName);
+        }
+
+        public async Task ProcessAsStringArrays()
+        {
+            await File.WriteAllLinesAsync(OutputFileName,
+                (await File.ReadAllLinesAsync(InputFileName))
+                .Select(x => x.ToUpper()));
+            File.Delete(InputFileName);
+        }
+
+        public async Task ProcessAsByteArray()
+        {
+            await File.WriteAllBytesAsync(OutputFileName,
+                (await File.ReadAllBytesAsync(InputFileName)).Reverse().ToArray());
+            File.Delete(InputFileName);
+        }
+    }
+}
