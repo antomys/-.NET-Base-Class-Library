501b9b530ff12a2c96c987bb4c0bd70ffd05610c
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-05-02 23:47:39 +0300

    Added module 6 project

---
+++ Working-with-Files-and-Streams/Reading-and-Writing-CSV-Data/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Reading_and_Writing_CSV_Data
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if(args.Length == 0)
+                return;
+            args[0].Init();
+        }
+        
+        private static void Init(this string directoryName)
+        {
+            if (!Directory.Exists(directoryName))
+                throw new ArgumentNullException(directoryName);
+            using var fileSystemWatcher = new FileSystemWatcher(directoryName)
+            {
+                IncludeSubdirectories = false, 
+                Filter = "*.*",
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite,
+                // Without this it will not generate events. By default - false.
+                EnableRaisingEvents = true
+            };
+
+            fileSystemWatcher.InitializeMethods();
+            
+            Console.WriteLine("Beginning to watch\nPress any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static void InitializeMethods(this FileSystemWatcher fileSystemWatcher)
+        {
+            fileSystemWatcher.Created += FileSystemWatcherExtension.FileSystemWatcherOnCreated;
+            fileSystemWatcher.Deleted += FileSystemWatcherExtension.FileSystemWatcherOnDeleted;
+            fileSystemWatcher.Renamed += FileSystemWatcherExtension.FileSystemWatcherOnRenamed;
+            fileSystemWatcher.Error += FileSystemWatcherExtension.FileSystemWatcherOnError;
+            fileSystemWatcher.Disposed += FileSystemWatcherExtension.FileSystemWatcherOnDisposed;
+            fileSystemWatcher.Changed += FileSystemWatcherExtension.FileSystemWatcherOnChanged;
+        }
+    }
+}
---
+++ Working-with-Files-and-Streams/Reading-and-Writing-CSV-Data/TextFileProcessor.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Reading_and_Writing_CSV_Data
+{
+    public class TextFileProcessor
+    {
+        private string InputFileName { get; }
+        private string OutputFileName { get; }
+
+        public TextFileProcessor(string inputFileName, string outputFileName)
+        {
+            InputFileName = inputFileName;
+            OutputFileName = outputFileName;
+        }
+        
+        public async Task ProcessFileStream()
+        {
+            await using var inputFileStream = new FileStream(InputFileName, FileMode.Open);
+            using var streamReader = new StreamReader(inputFileStream);
+            await using var outputFileStream = new FileStream(OutputFileName, FileMode.Create);
+            await using var streamWriter = new StreamWriter(outputFileStream);
+
+            var content = await streamReader.ReadToEndAsync();
+            await streamWriter.WriteAsync(content.ToUpper());
+            File.Delete(InputFileName);
+        }
+        
+        public async Task ProcessFileStreamLines()
+        {
+            await using var inputFileStream = new FileStream(InputFileName, FileMode.Open);
+            using var streamReader = new StreamReader(inputFileStream);
+            await using var outputFileStream = new FileStream(OutputFileName, FileMode.Create);
+            await using var streamWriter = new StreamWriter(outputFileStream);
+            
+            try
+            {
+                var initialLine = 0;
+                while (!streamReader.EndOfStream)
+                {
+                    var content = await streamReader.ReadLineAsync() ?? string.Empty;
+                    if (initialLine % 2 == 0)
+                        await streamWriter.WriteLineAsync(content.ToUpper());
+                    else
+                    {
+                        await streamWriter.WriteLineAsync(content.ToLower());
+                    }
+                    initialLine++;
+                }
+            }
+            finally
+            { 
+                File.Delete(InputFileName);
+            }
+        }
+        
+        
+
+        public async Task ProcessByteFileStream()
+        {
+            await using var inputFileStream = File.Open(InputFileName, FileMode.Open, FileAccess.Read);
+            await using var outputFileStream = File.Create(OutputFileName);
+
+            const int endOfStream = -1;
+            var largest = 0;
+            var block = inputFileStream.ReadByte();
+            while(block != endOfStream)
+            {
+                if (block > largest)
+                    largest = block;
+                outputFileStream.WriteByte((byte) block);
+                
+                block = inputFileStream.ReadByte();
+            }
+            outputFileStream.WriteByte((byte) largest);
+            
+            File.Delete(InputFileName);
+        }
+
+        public async Task ProcessCsvFile()
+        {
+            
+        }
+    }
+}
