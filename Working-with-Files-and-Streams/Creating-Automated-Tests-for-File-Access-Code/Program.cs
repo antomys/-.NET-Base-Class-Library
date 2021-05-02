5036dc8fbc9e748cdf96e22618a088a35c7eb77f
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-05-03 00:34:19 +0300

    Added initial module 7 project

---
+++ Working-with-Files-and-Streams/Creating-Automated-Tests-for-File-Access-Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Creating_Automated_Tests_for_File_Access_Code
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
