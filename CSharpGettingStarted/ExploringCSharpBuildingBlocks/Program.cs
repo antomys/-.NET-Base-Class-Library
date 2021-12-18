959b862457d53295278a32716e7a856e2c21e0d7
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-12-18 21:10:58 +0200

    Added certificate for TheBigPicture and CSharpGettingStarted initial

---
+++ CSharpGettingStarted/ExploringCSharpBuildingBlocks/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExploringCSharpBuildingBlocks;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello world!");
+        Console.WriteLine(ToFahrenheit(-5));
+    }
+
+    private static float ToFahrenheit(float celsius) => (celsius - 32) / 1.8f;
+} 
