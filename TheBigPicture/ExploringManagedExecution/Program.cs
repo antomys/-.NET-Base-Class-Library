2929ddde0cf9c6f2ddb7a11cf2be93fa7cab8e55
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-12-18 19:15:47 +0200

    Added Putting CL in CLR and Exploring managed execution.

---
+++ TheBigPicture/ExploringManagedExecution/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExploringManagedExecution;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Main called. Enter to call next");
+        Console.ReadLine();
+        
+        var sum = Add(30,12);
+
+        Console.WriteLine(sum);
+        Console.WriteLine("Enter to exit.");
+        Console.ReadLine();
+    }
+    private static int Add(int a, int b) => a+b;
+}
---
+++ TheBigPicture/PuttingCLinCLR/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PuttingCLinCLR;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        const int a = 30;
+        const int b = 12;
+        var sum = FSharpLibrary.Calculator.Add(a, b);
+        Console.WriteLine(sum);
+    }
+}
