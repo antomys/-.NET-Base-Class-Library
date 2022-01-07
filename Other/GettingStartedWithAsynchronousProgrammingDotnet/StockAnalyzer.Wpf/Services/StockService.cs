8fce3efcdb77a600dea23c22b8429906ceb7423c
Ihor Volokhovych <igorvolokhovych@gmail.com>
2022-01-08 00:30:55 +0200

    Added certificate for async programming course

--- Other/GettingStartedWithAsynchronousProgrammingDotnet/StockAnalyzer.Wpf/Services/StockService.cs
+++ Other/GettingStartedWithAsynchronousProgrammingDotnet/StockAnalyzer.Wpf/Services/StockService.cs
@@ -5,25 +5,23 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using StockAnalyzer.Core.Models;
 
-namespace StockAnalyzer.Wpf.Services
-{
-    public class StockService
-    {        
-        private static readonly JsonSerializerOptions Options = new()
-        {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-        };
+namespace StockAnalyzer.Wpf.Services;
+
+public class StockService
+{        
+    private static readonly JsonSerializerOptions Options = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
         
-        public static async Task<IEnumerable<StockPrice>?> GetStockPricesFor(string ticker)
-        {
-            using var client = new HttpClient();
-            var result = await client.GetAsync($"http://localhost:7168/api/stocks/{ticker}");
+    public static async Task<IEnumerable<StockPrice>?> GetStockPricesFor(string ticker)
+    {
+        using var client = new HttpClient();
+        var result = await client.GetAsync($"http://localhost:7168/api/stocks/{ticker}");
 
-            result.EnsureSuccessStatusCode();
+        result.EnsureSuccessStatusCode();
+        var content = await result.Content.ReadFromJsonAsync<IEnumerable<StockPrice>>(Options);
 
-            var content = await result.Content.ReadFromJsonAsync<IEnumerable<StockPrice>>(Options);
-
-            return content;
-        }
+        return content;
     }
 }
